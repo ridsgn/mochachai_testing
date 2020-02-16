fad433689d459650e6e411eeb8c563bf9684cb93
rizky <rizukifirumanshaa96@gmail.com>
2020-02-17 00:00:05 +0700

    add another testing method

--- test/appTest.js
+++ test/appTest.js
@@ -1,8 +1,20 @@
 const assert = require('chai').assert;
-const app = require('../app');
+const sayHello = require('../app').sayHello;
+const addNumbers = require('../app').addNumbers;
+//const app = require('../app');
 
 describe('App', function() {
 	it('app should return hello', function() {
-		assert.equal(app(), 'hello');
+		assert.equal(sayHello(), 'hello');
+	});
+
+	it('sayHello should return type string', function() {
+		let result = sayHello();
+		assert.typeOf(result, 'string');
+	});
+
+	it('addNumbers should be above 5', function() {
+		let result = addNumbers(5, 5);
+		assert.isAbove(result, 5);
 	});
 });
