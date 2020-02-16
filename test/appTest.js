9bcc06ec4e2861d09c1fa9a341fcdc01dec95054
rizky <rizukifirumanshaa96@gmail.com>
2020-02-17 00:04:19 +0700

    test grouping function

--- test/appTest.js
+++ test/appTest.js
@@ -1,20 +1,27 @@
 const assert = require('chai').assert;
-const sayHello = require('../app').sayHello;
-const addNumbers = require('../app').addNumbers;
-//const app = require('../app');
+// const sayHello = require('../app').sayHello;
+// const addNumbers = require('../app').addNumbers;
+const app = require('../app');
+
+sayHelloResult = app.sayHello();
+addNumbersResult = app.addNumbers(5, 5);
 
 describe('App', function() {
-	it('app should return hello', function() {
-		assert.equal(sayHello(), 'hello');
+	describe('sayHello()', function() {
+		it('app should return hello', function() {
+			assert.equal(sayHelloResult, 'hello');
+		});
+
+		it('sayHello should return type string', function() {
+			//let result = sayHello();
+			assert.typeOf(sayHelloResult, 'string');
+		});
 	});
 
-	it('sayHello should return type string', function() {
-		let result = sayHello();
-		assert.typeOf(result, 'string');
-	});
-
-	it('addNumbers should be above 5', function() {
-		let result = addNumbers(5, 5);
-		assert.isAbove(result, 5);
+	describe('addNumbers()', function() {
+		it('addNumbers should be above 5', function() {
+			//let result = addNumbers(5, 5);
+			assert.isAbove(addNumbersResult, 5);
+		});
 	});
 });
